fc657940fad01c943e6b4dd608696e4bf6519785
Ana Preda <preda_ana20@yahoo.com>
2019-01-07 16:26:17 +0200

    SkillArea added

---
+++ Mitblick-Persistence/src/main/java/skills/dao/SkillAreaPersistenceManager.java
@@ -0,0 +1,39 @@
+package skills.dao;
+
+import skills.entities.SkillArea;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Optional;
+
+public class SkillAreaPersistenceManager {
+    @PersistenceContext(unitName = "mitblick-persistence")
+    private EntityManager em;
+
+
+    public SkillArea create(SkillArea skillArea) {
+        em.persist(skillArea);
+        em.flush();
+        return skillArea;
+    }
+
+    public void update(SkillArea skillArea) {
+        em.merge(skillArea);
+    }
+
+    public void delete(SkillArea skillArea) {
+        em.remove(skillArea);
+    }
+
+    public Optional<SkillArea> getByName(String name) {
+        TypedQuery<SkillArea> q = em.createNamedQuery(SkillArea.GET_SKILLAREA_BY_NAME, SkillArea.class)
+                .setParameter("name", name);
+        try {
+            return Optional.of(q.getSingleResult());
+        } catch (NoResultException ex) {
+            return Optional.empty();
+        }
+    }
+}
---
+++ mitblick-business/src/main/java/skills/service/SkillAreaManagementService.java
@@ -0,0 +1,4 @@
+package skills.service;
+
+public class SkillAreaManagementService {
+}
