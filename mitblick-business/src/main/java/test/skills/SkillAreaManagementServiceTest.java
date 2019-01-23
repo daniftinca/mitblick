91dd2ec2095c06e4c4f6039c730cd374d56a2d7d
Ana Preda <preda_ana20@yahoo.com>
2019-01-23 18:54:05 +0200

    Test for delete SkillArea when expecting validation error, added

--- mitblick-business/src/main/java/test/skills/SkillAreaManagementServiceTest.java
+++ mitblick-business/src/main/java/test/skills/SkillAreaManagementServiceTest.java
@@ -1,11 +1,54 @@
 package test.skills;
 
-import static org.junit.jupiter.api.Assertions.*;
+import exception.BusinessException;
+import exception.ExceptionCode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import skills.dao.SkillAreaPersistenceManager;
+import skills.dto.SkillAreaDTO;
+import skills.entities.Skill;
+import skills.entities.SkillArea;
+import skills.service.SkillAreaManagementService;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 class SkillAreaManagementServiceTest {
 
+    @InjectMocks
+    private SkillAreaManagementService skillAreaManagementController;
+
+    @Mock
+    private SkillAreaPersistenceManager skillAreaPersistenceManager;
+
+
     @org.junit.jupiter.api.Test
-    void deleteSkillArea() {
+    public void deleteSkillArea_Expected_SkillAreaValidationException() {
+        SkillAreaDTO skillAreaDTO = new SkillAreaDTO();
+        skillAreaDTO.setName("dev");
+        skillAreaDTO.setDescription("development skills");
+
+        when(skillAreaPersistenceManager.getByName(any(String.class)))
+                .thenReturn(Optional.empty());
+
+        try{
+            skillAreaManagementController.deleteSkillArea(skillAreaDTO);
+            fail("Should not reach this point");
+        }
+        catch (BusinessException e){
+            assertEquals(ExceptionCode.SKILLAREA_VALIDATION_EXCEPTION, e.getExceptionCode());
+        }
+
+
+
     }
 
     @org.junit.jupiter.api.Test
@@ -15,4 +58,5 @@ class SkillAreaManagementServiceTest {
     @org.junit.jupiter.api.Test
     void removeSkillFromSkillArea() {
     }
+
 }
