89d8c6810f3cbc9aa28800cbcb5384ba1730e8fb
tuddy <tuddy18_26@yahoo.com>
2019-01-24 17:44:24 +0200

    add project dialog

---
+++ mitblickFE/mitblickFE/src/app/user/add-project-dialog/add-project-dialog.component.ts
@@ -0,0 +1,32 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {ProfileComponent} from "../profile/profile.component";
+
+export interface ProjectDialogData {
+  name: string;
+  client: string;
+  branch: string;
+  description: string;
+  date: Date;
+}
+
+@Component({
+  selector: 'app-add-project-dialog',
+  templateUrl: './add-project-dialog.component.html',
+  styleUrls: ['./add-project-dialog.component.scss']
+})
+export class AddProjectDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ProfileComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ProjectDialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
