0276d5532ef6a154a6f702a60e466b52685f96ef
Ana Preda <preda_ana20@yahoo.com>
2019-01-26 21:58:33 +0200

    Add employee to supervisor and remove employee from supervisor added.

--- Mitblick-Client/src/main/java/user/boundary/EmployeeManagementBoundary.java
+++ Mitblick-Client/src/main/java/user/boundary/EmployeeManagementBoundary.java
@@ -1,4 +1,42 @@
 package user.boundary;
 
+import exception.BusinessException;
+import user.service.EmployeeManagementService;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/manage-employees")
 public class EmployeeManagementBoundary {
+
+    @EJB
+    private EmployeeManagementService employeeManagementService;
+
+    @POST
+    @Path("/add-employee-to-supervisor")
+    @Consumes({"application/x-www-form-urlencoded"})
+    public Response addEmployeeToSupervisor(@FormParam("supervisorEmail") String supervisorEmail,@FormParam("employeeEmail") String employeeEmail){
+        try {
+            employeeManagementService.addEmployeeToSupervisor(supervisorEmail,employeeEmail);
+            return Response.status(Response.Status.OK).build();
+        }catch (BusinessException e){
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getExceptionCode()).build();
+        }
+    }
+
+    @POST
+    @Path("/remove-employee-from-supervisor")
+    @Consumes({"application/x-www-form-urlencoded"})
+    public Response removeEmployeeFromSupervisor(@FormParam("supervisorEmail") String supervisorEmail,@FormParam("employeeEmail") String employeeEmail){
+        try {
+            employeeManagementService.removeEmployeeFromSupervisor(supervisorEmail,employeeEmail);
+            return Response.status(Response.Status.OK).build();
+        }catch (BusinessException e){
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getExceptionCode()).build();
+        }
+    }
 }
--- mitblick-business/src/main/java/user/service/EmployeeManagementService.java
+++ mitblick-business/src/main/java/user/service/EmployeeManagementService.java
@@ -1,6 +1,64 @@
 package user.service;
 
+import exception.BusinessException;
+import exception.ExceptionCode;
+import user.dao.UserPersistenceManager;
+import user.entities.User;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Stateless
 public class EmployeeManagementService {
+
+    @EJB
+    UserPersistenceManager userPersistenceManager;
+
+    public void addEmployeeToSupervisor(String supervisorEmail, String employeeEmail) throws BusinessException{
+
+        Optional<User> supervisorOptional = userPersistenceManager.getUserByEmail(supervisorEmail);
+        Optional<User> employeeOptional = userPersistenceManager.getUserByEmail(employeeEmail);
+
+        if(supervisorOptional.isPresent() && employeeOptional.isPresent()){
+            User supervisor = supervisorOptional.get();
+            User employee = employeeOptional.get();
+            validateSupervisor(supervisor);
+            supervisor.getEmployees().add(employee);
+            employee.setSupervisorMail(supervisorEmail);
+
+        }else
+            throw new BusinessException(ExceptionCode.USER_VALIDATION_EXCEPTION);
+    }
+
+    public void validateSupervisor(User supervisor) throws BusinessException{
+        AtomicBoolean ok = new AtomicBoolean(false);
+        supervisor.getRoles().forEach(role -> {
+            role.getPermissions().forEach(permission -> {
+                if(permission.getType() == "PROFILE_REVIEW")
+                    ok.set(true);
+            });
+        });
+        if(ok.get()==false)
+            throw new BusinessException(ExceptionCode.USER_PERMISSION_VALIDATION);
+    }
+
+    public void removeEmployeeFromSupervisor(String supervisorEmail, String employeeEmail)throws BusinessException{
+        Optional<User> supervisorOptional = userPersistenceManager.getUserByEmail(supervisorEmail);
+        Optional<User> employeeOptional = userPersistenceManager.getUserByEmail(employeeEmail);
+        if(supervisorOptional.isPresent() && employeeOptional.isPresent()){
+            User supervisor = supervisorOptional.get();
+            User employee = employeeOptional.get();
+            validateSupervisor(supervisor);
+            if(supervisor.getEmployees().contains(employee) && employee.getSupervisorMail() == supervisorEmail){
+                supervisor.getEmployees().remove(employee);
+                employee.setSupervisorMail(null);
+            }else
+                throw new BusinessException(ExceptionCode.USER_VALIDATION_EXCEPTION);
+        }else
+            throw new BusinessException(ExceptionCode.USER_VALIDATION_EXCEPTION);
+    }
 
 
 }
